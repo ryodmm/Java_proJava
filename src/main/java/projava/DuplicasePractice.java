d3b5554864c917ef6a57bc7b1c3c72a1b66d309a
onoderaryo <ryo.program.school@gmail.com>
2023-02-01 14:40:59 +0900

    Java_hard_loop2

--- src/main/java/projava/DuplicasePractice.java
+++ src/main/java/projava/DuplicasePractice.java
@@ -1,2 +1,21 @@
-package projava;public class DuplicasePractice {
+package projava;
+
+import java.util.Arrays;
+public class DuplicasePractice {
+    public static void main(String[] args) {
+        var data = new int[]{3, 6, 9, 4, 2, 1, 5};
+
+        var result = new int[data.length];
+        for (int i = 0; i < data.length; ++i) {
+            if (i < data.length - 1) {
+                result[i] = Math.max(data[i], data[i + 1]);
+            } else {
+                // 最後の要素
+                result[i] = data[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(data));
+        System.out.println(Arrays.toString(result));
+    }
 }
--- src/main/java/projava/RunLengthCompression.java
+++ src/main/java/projava/RunLengthCompression.java
@@ -1,2 +1,48 @@
-package projava;public class RunLengthCompression {
+package projava;
+
+public class RunLengthCompression {
+    public static void main(String[] args) {
+        // −１を基準となる数値として扱う
+        // finalを変数の宣言につけると変更のできない変数となる（定数）
+        final var COUNTER_BASE = -1;
+        var data = "abbcccbaaaabccccccccccccddd";
+
+        var count = COUNTER_BASE;
+        char prev = 0;
+        var builder = new StringBuilder();
+        // １文字づつ処理するループ
+        for (var ch : data.toCharArray()) {
+            // おなじ文字がつづくとき変数countの値を増やす
+            if (prev == ch) {
+                count++;
+                //countが9まできたら、9を出力
+                if (count == 9) {
+                    builder.append('9');
+                    // 9の後は改めて出力するので変数countをCOUNTER_BASEに、１つ前の文字を０にする
+                    count = COUNTER_BASE;
+                    prev = 0;
+                }
+                //違う文字がきた時の処理
+            } else {
+                // 変数countの値が０以上であれば、直前に同じ文字が続いてたことになり、数字を追加する
+                // countの値はCOUNTER_BASEに戻す
+                if (count >= 0) {
+                    builder.append((char) ('0' + count));
+                    count = COUNTER_BASE;
+                }
+                // 現在の文字をbuilderに追加して、１つ前の文字として
+                // 現在の文字を変数prevに割り当てる
+                builder.append(ch);
+                prev = ch;
+            }
+        }
+        // ループを抜けた時に最後の文字が連続していれば吸うぞ出力します。
+        if (count >= 0){
+            builder.append((char)('0' + count));
+        }
+        var result = builder.toString();
+        System.out.println(data);
+        System.out.println(result);
+    }
 }
+
