8c879a4ca664e3b32cb8122940c404d382cbaa32
onoderaryo <ryo.program.school@gmail.com>
2023-02-20 23:47:33 +0900

    ramdom_number

--- src/main/java/game/hitandblow.java
+++ src/main/java/game/hitandblow.java
@@ -7,8 +7,27 @@ import java.io.InputStreamReader;
 public class hitandblow {
     public static void main(String[] args) throws IOException {
         // コンソールで入力した値を扱う
+        /*
         BufferedReader i = new BufferedReader(new InputStreamReader(System.in));
         String a = i.readLine();
         System.out.println(a);
+
+         */
+
+        int[] number = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        // 配列の中身をシャッフル
+        for (int j = number.length; - j > 0; j--) {
+            int r = (int) (Math.random() * (j + 1));
+            int tmp = number[j];
+            number[j] = number[r];
+            number[r] = tmp;
+
+            // シャッフル動作確認
+            for (int check : number) {
+                System.out.println(check);
+            }
+            System.out.println();
+        }
     }
 }
